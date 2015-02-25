1cab093ffdb6f57c4d1a743eabd8ce850bc4490d
Fenyvesi Tamás <a.fenyvesi.tamas@gmail.com>
2015-02-25 20:24:06 +0100

    executeShouldReturnBuzzIfTheNumberIsDividableBy5 works too and if not give back the right number

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -5,8 +5,10 @@ public class FizzBuzzer {
     public String execute(int number) {
         if (number % 3 == 0) {
             return "fizz";
+        } else if (number % 5 == 0) {
+            return "buzz";
         }
-        return "buzz";
+        return String.valueOf(number);
     }
     
 }
