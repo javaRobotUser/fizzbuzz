8096cb6acaa90627a5873d96dd55ffe5716bf9a2
Fenyvesi Tamás <a.fenyvesi.tamas@gmail.com>
2015-02-25 20:26:25 +0100

    all 4 test executeShouldReturnFizzIfTheNumberIsDividableBy3 executeShouldReturnBuzzIfTheNumberIsDividableBy5 executeShouldReturnBuzzIfTheNumberIsDividableBy15 executeShouldReturnTheSameNumberIfNoOtherRequirementIsFulfilled
    successfully passed !

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -1,14 +1,16 @@
 package com.wcs.fizzbuzz;
 
 public class FizzBuzzer {
-    
+
     public String execute(int number) {
-        if (number % 3 == 0) {
+        if (number % 3 == 0 && number % 5 == 0) {
+             return "fizzbuzz";
+        } else if (number % 3 == 0) {
             return "fizz";
         } else if (number % 5 == 0) {
             return "buzz";
         }
         return String.valueOf(number);
     }
-    
+
 }
